26737f8ac37a1021c9a659e976d2c4f3f432b6e4
0utb0und3r <0utb0und3r@gmail.com>
2026-06-25 13:05:46 +0100

    update: player set to horizontal navigation only

--- main.lua
+++ main.lua
@@ -16,18 +16,22 @@ end
 
 function love.update(dt)
     -- Update game state, handle input, etc.
-    if love.keyboard.isDown("up") then
-        player.y = player.y - player.speed * dt
-    end
-    if love.keyboard.isDown("down") then
-        player.y = player.y + player.speed * dt
-    end
+    -- remove vertical navigation
+    -- if love.keyboard.isDown("up") then
+    --     player.y = player.y - player.speed * dt
+    -- end
+    -- if love.keyboard.isDown("down") then
+    --     player.y = player.y + player.speed * dt
+    -- end
+
+    -- Horizontal movement
     if love.keyboard.isDown("left") then
         player.x = player.x - player.speed * dt
     end
     if love.keyboard.isDown("right") then
         player.x = player.x + player.speed * dt
     end
+    
 end
 
 function love.draw()
